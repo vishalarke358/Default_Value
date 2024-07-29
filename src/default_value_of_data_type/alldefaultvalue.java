5b15cd00335356564dbca5b1ad20a7ef6591164a
vishalarke358 <vishalarke358@gmail.com>
2024-07-29 15:23:44 +0530

    Default Value

---
+++ src/default_value_of_data_type/alldefaultvalue.java
@@ -0,0 +1,41 @@
+// To show the default value of all data type.
+
+package default_value_of_data_type;
+
+public class alldefaultvalue {
+
+
+	 boolean a;
+	 char b;
+	 byte c;
+	 short d;
+	 int e;
+	 long f;
+	 float g;
+	 double h;
+	 int [] i;
+	String j;
+	
+	
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+
+				
+				alldefaultvalue k=new alldefaultvalue ();
+				
+				System.out.println("Dtifferent of value of all types of data type:- ");
+				
+				System.out.println("\n1) The default value of boolean= " +k.a);
+				System.out.println("\n2) The default value of char= " +k.b);
+				System.out.println("\n3) The default value of byte= " +k.c);
+				System.out.println("\n4) The default value of short= " +k.d);
+				System.out.println("\n5) The default value of int= " +k.e);
+				System.out.println("\n6) The default value of long= " +k.f);
+				System.out.println("\n7) The default value of float= " +k.g);
+				System.out.println("\n8) The default value of double= " +k.h);
+				System.out.println("\n9) The default value of int [] = " +k.i);
+				System.out.println("\n10) The default value of String= " +k.j);
+				}
+
+		}
